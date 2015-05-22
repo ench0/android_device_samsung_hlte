131fdd917fc1a420b0361381240977a99623bab7
Ench0 <ench0@users.noreply.github.com>
2015-05-22 12:59:09 +0100

    Update and rename flex.mk to nameless.mk

---
+++ nameless.mk
@@ -0,0 +1,13 @@
+$(call inherit-product, device/samsung/hlte/full_hlte.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/nameless/config/nfc_enhanced.mk)
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/nameless/config/common_full_phone.mk)
+
+PRODUCT_DEVICE := hlte
+PRODUCT_NAME := nameless_hlte
+
+PRODUCT_COPY_FILES += \
+    vendor/nameless/prebuilt/common/bootanimation/bootanimation.zip:system/media/bootanimation.zip
