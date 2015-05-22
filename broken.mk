2394370c1f37d1ea2f2d1f9d3d20d66904a29b14
Ench0 <ench0@users.noreply.github.com>
2015-05-22 17:18:50 +0100

    Update broken.mk

--- broken.mk
+++ broken.mk
@@ -1,13 +1,14 @@
 $(call inherit-product, device/samsung/hlte/full_hlte.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/broken/config/nfc_enhanced.mk)
+#$(call inherit-product, vendor/carbon/config/nfc_enhanced.mk)
 
 # Inherit some common Broken stuff.
-$(call inherit-product, vendor/broken/config/common_full_phone.mk)
+$(call inherit-product, vendor/carbon/config/common_phone.mk)
 
 # Not sure we need it
-$(call inherit-product, vendor/broken/config/gsm.mk)
+$(call inherit-product, vendor/carbon/config/cm_audio.mk)
+$(call inherit-product, vendor/carbon/config/themes_common.mk)
 
 PRODUCT_DEVICE := hlte
-PRODUCT_NAME := broken_hlte
+PRODUCT_NAME := carbon_hlte
