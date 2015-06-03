0187db04fb5b23c3fa58516e2debcd235042954f
Ench0 <ench0@users.noreply.github.com>
2015-06-03 15:26:15 +0100

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -38,7 +38,7 @@ TARGET_USERIMAGES_USE_EXT4 := true
 #BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=bacon user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3 androidboot.bootdevice=msm_sdcc.1 androidboot.selinux=permissive
 
 # Kernel Toolchain
-TARGET_GCC_VERSION_ARM := 4.9-sabernaro
+#TARGET_GCC_VERSION_ARM := 4.9-sabernaro
 
 # Rom Toolchain
 ## TARGET_GCC_VERSION_EXP := 4.9
