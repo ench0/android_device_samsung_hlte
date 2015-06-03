583af20b9da3c8e7e667be0e39141e0a1b48c873
Ench0 <ench0@users.noreply.github.com>
2015-06-03 15:13:55 +0100

    validusify

--- eos.mk
+++ eos.mk
@@ -1,15 +1,20 @@
 $(call inherit-product, device/samsung/hlte/full_hlte.mk)
 
 # Enhanced NFC
-#$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/validus/config/nfc_enhanced.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/eos/config/common_full_phone.mk)
-$(call inherit-product, vendor/eos/config/common_versions.mk)
-$(call inherit-product, vendor/eos/config/themes_common.mk)
+$(call inherit-product, vendor/validus/config/common_full_phone.mk)
+$(call inherit-product, vendor/validus/config/common_versions.mk)
+$(call inherit-product, vendor/validus/config/themes_common.mk)
+$(call inherit-product, vendor/validus/config/cm_audio.mk)
 
 PRODUCT_DEVICE := hlte
-PRODUCT_NAME := eos_hlte
+PRODUCT_NAME := validus_hlte
 
-PRODUCT_COPY_FILES += \
-    vendor/eos/prebuilt/common/bootanimations/BOOTANIMATION-1080x1920.zip:system/media/bootanimation.zip
+#PRODUCT_COPY_FILES += \
+#    vendor/eos/prebuilt/common/bootanimations/BOOTANIMATION-1080x1920.zip:system/media/bootanimation.zip
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1920
+TARGET_SCREEN_WIDTH := 1080
