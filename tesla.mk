46cc10d3403efc9c8cf7284cdee9c4a8e1235261
Ench0 <ench0@users.noreply.github.com>
2015-06-05 15:44:45 +0100

    teslify

---
+++ tesla.mk
@@ -0,0 +1,20 @@
+$(call inherit-product, device/samsung/hlte/full_hlte.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/tesla/config/nfc_enhanced.mk)
+
+# Inherit some common Broken stuff.
+$(call inherit-product, vendor/tesla/config/common_full_phone.mk)
+
+# Not actually needed, includes stk (sim toolkit) that is already included in the one just above :)
+#$(call inherit-product, vendor/tesla/config/gsm.mk)
+
+PRODUCT_DEVICE := hlte
+PRODUCT_NAME := tesla_hlte
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1920
+TARGET_SCREEN_WIDTH := 1080
+
+# Release name
+PRODUCT_RELEASE_NAME := hlte
