d1ca3e8439c7000f3b6d3583fda40021ed56e37a
Ench0 <ench0@users.noreply.github.com>
2015-06-12 16:09:02 +0100

    Update omni_hlte.mk

--- omni_hlte.mk
+++ omni_hlte.mk
@@ -1,10 +1,10 @@
 $(call inherit-product, device/samsung/hlte/full_hlte.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/omni/config/nfc_enhanced.mk)
+#$(call inherit-product, vendor/omni/config/nfc_enhanced.mk)
 
 # Inherit some common Broken stuff.
-$(call inherit-product, vendor/omni/config/common_full_phone.mk)
+$(call inherit-product, vendor/omni/config/common.mk)
 
 # Not sure we need it
 $(call inherit-product, vendor/omni/config/gsm.mk)
