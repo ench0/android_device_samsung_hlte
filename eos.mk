1e99acb621bd454f68d053bb62c7e130bb237614
Ench0 <ench0@users.noreply.github.com>
2015-06-29 15:21:37 +0100

    Update eos.mk

--- eos.mk
+++ eos.mk
@@ -1,15 +1,13 @@
 $(call inherit-product, device/samsung/hlte/full_hlte.mk)
 
 # Enhanced NFC
-#$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/eos/config/common_full_phone.mk)
-$(call inherit-product, vendor/eos/config/common_versions.mk)
-$(call inherit-product, vendor/eos/config/themes_common.mk)
 
 PRODUCT_DEVICE := hlte
 PRODUCT_NAME := eos_hlte
 
-PRODUCT_COPY_FILES += \
-    vendor/eos/prebuilt/common/bootanimations/BOOTANIMATION-1080x1920.zip:system/media/bootanimation.zip
+#PRODUCT_COPY_FILES += \
+#    vendor/eos/prebuilt/common/bootanimations/BOOTANIMATION-1080x1920.zip:system/media/bootanimation.zip
