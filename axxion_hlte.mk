c77a259817dff38bf00280d159b362e92f2d6d8c
Ench0 <ench0@users.noreply.github.com>
2015-07-14 12:33:30 +0100

    Update axxion_hlte.mk

--- axxion_hlte.mk
+++ axxion_hlte.mk
@@ -4,10 +4,10 @@ $(call inherit-product, device/samsung/hlte/full_hlte.mk)
 #$(call inherit-product, vendor/omni/config/nfc_enhanced.mk)
 
 # Inherit some common Broken stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/axxion/config/common.mk)
 
 # Not sure we need it
-$(call inherit-product, vendor/omni/config/gsm.mk)
+$(call inherit-product, vendor/axxion/config/gsm.mk)
 
 PRODUCT_DEVICE := hlte
 PRODUCT_NAME := axxion_hlte
