5adffaa3e6ea96bb9af89ac996887b05955c8111
Ench0 <ench0@users.noreply.github.com>
2015-07-17 20:16:24 +0100

    Update and rename minimal.mk to minimal_hlte.mk

--- minimal_hlte.mk
+++ minimal_hlte.mk
@@ -1,10 +1,18 @@
 $(call inherit-product, device/samsung/hlte/full_hlte.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/minimal/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/minimal/configs/nfc_enhanced.mk)
 
-# Inherit some common Minimal stuff.
-$(call inherit-product, vendor/minimal/config/common_full_phone.mk)
+# Inherit common product files.
+$(call inherit-product, vendor/minimal/products/common.mk)
+
+# Telephony
+$(call inherit-product, vendor/minimal/configs/telephony.mk)
+
+$(call inherit-product, vendor/minimal/configs/gsm.mk)
+
+# Inherit common build.prop overrides
+-include vendor/minimal/products/common_versions.mk
 
 # Not actually needed, includes stk (sim toolkit) that is already included in the one just above :)
 #$(call inherit-product, vendor/tesla/config/gsm.mk)
