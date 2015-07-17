32cd96f1052ea54f0eca8d2d80713269375297a0
Ench0 <ench0@users.noreply.github.com>
2015-07-17 22:32:59 +0100

    Update minimal_hlte.mk

--- minimal_hlte.mk
+++ minimal_hlte.mk
@@ -12,23 +12,23 @@ $(call inherit-product, vendor/minimal/configs/telephony.mk)
 $(call inherit-product, vendor/minimal/configs/gsm.mk)
 
 # Inherit common build.prop overrides
-#-include vendor/minimal/products/common_versions.mk
-
-# Not actually needed, includes stk (sim toolkit) that is already included in the one just above :)
-#$(call inherit-product, vendor/tesla/config/gsm.mk)
-
-PRODUCT_DEVICE := hlte
-PRODUCT_NAME := minimal_hlte
+-include vendor/minimal/products/common_versions.mk
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1080
 
+TARGET_CONTINUOUS_SPLASH_ENABLED := true
+
+
+PRODUCT_DEVICE := hlte
+PRODUCT_NAME := minimal_hlte
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := SAMSUNG-SM-N9005
+PRODUCT_MANUFACTURER := samsung
+
 # Release name
 PRODUCT_RELEASE_NAME := hlte
 
 # Set build fingerprint / ID / Product Name ect.
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=hlte TARGET_DEVICE=hlte BUILD_FINGERPRINT="samsung/hlte/hlte:4.4.2/JZO54K/I317UCAMA4:user/release-keys" PRIVATE_BUILD_DESC="hlte-user 4.4.2 JZO54K I317UCAMA4 release-keys"
-
-
-TARGET_CONTINUOUS_SPLASH_ENABLED := true
+#PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=hlte TARGET_DEVICE=hlte BUILD_FINGERPRINT="samsung/hlte/hlte:4.4.2/JZO54K/I317UCAMA4:user/release-keys" PRIVATE_BUILD_DESC="hlte-user 4.4.2 JZO54K I317UCAMA4 release-keys"
