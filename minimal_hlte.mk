339223a03667b1b7965bb09438cd7ca24952d699
Ench0 <ench0@users.noreply.github.com>
2015-07-18 11:32:48 +0100

    camera and gallery

--- minimal_hlte.mk
+++ minimal_hlte.mk
@@ -32,3 +32,7 @@ PRODUCT_RELEASE_NAME := hlte
 
 # Set build fingerprint / ID / Product Name ect.
 #PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=hlte TARGET_DEVICE=hlte BUILD_FINGERPRINT="samsung/hlte/hlte:4.4.2/JZO54K/I317UCAMA4:user/release-keys" PRIVATE_BUILD_DESC="hlte-user 4.4.2 JZO54K I317UCAMA4 release-keys"
+
+PRODUCT_PACKAGES += \
+    Camera2 \
+    Gallery2
