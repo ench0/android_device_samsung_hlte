11648cd483c1c79aee1db34728024802b9675293
Ench0 <ench0@users.noreply.github.com>
2015-07-19 10:20:54 +0100

    Update minimal_hlte.mk

--- minimal_hlte.mk
+++ minimal_hlte.mk
@@ -20,11 +20,11 @@ TARGET_SCREEN_WIDTH := 1080
 
 
 # Copy hammerhead specific prebuilt files
-PRODUCT_COPY_FILES +=  \
-    vendor/minimal/proprietary/tuna/media/bootanimation.zip:system/media/bootanimation.zip \
-    vendor/minimal/proprietary/tuna/media/audio/notifications/Nexus.mp3:system/media/audio/notifications/Nexus.mp3 \
-    vendor/minimal/proprietary/common/media/LMprec_508.emd:system/media/LMprec_508.emd \
-    vendor/minimal/proprietary/common/media/PFFprec_600.emd:system/media/PFFprec_600.emd
+#PRODUCT_COPY_FILES +=  \
+#    vendor/minimal/proprietary/tuna/media/bootanimation.zip:system/media/bootanimation.zip \
+#    vendor/minimal/proprietary/tuna/media/audio/notifications/Nexus.mp3:system/media/audio/notifications/Nexus.mp3 \
+#    vendor/minimal/proprietary/common/media/LMprec_508.emd:system/media/LMprec_508.emd \
+#    vendor/minimal/proprietary/common/media/PFFprec_600.emd:system/media/PFFprec_600.emd
 
 PRODUCT_DEVICE := hlte
 PRODUCT_NAME := minimal_hlte
@@ -39,6 +39,6 @@ PRODUCT_RELEASE_NAME := SM-N9005
 # Set build fingerprint / ID / Product Name ect.
 #PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=hlte TARGET_DEVICE=hlte BUILD_FINGERPRINT="samsung/hlte/hlte:4.4.2/JZO54K/I317UCAMA4:user/release-keys" PRIVATE_BUILD_DESC="hlte-user 4.4.2 JZO54K I317UCAMA4 release-keys"
 
-PRODUCT_PACKAGES += \
-    Camera2 \
-    Gallery2
+#PRODUCT_PACKAGES += \
+#    Camera2 \
+#    Gallery2
