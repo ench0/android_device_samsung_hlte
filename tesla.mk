42a2e05027c21b79ac9e4bcd048df7e9e7195690
Ench0 <ench0@users.noreply.github.com>
2015-07-25 21:47:17 +0100

    exodify

--- tesla.mk
+++ tesla.mk
@@ -1,16 +1,13 @@
 $(call inherit-product, device/samsung/hlte/full_hlte.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/tesla/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/exodus/config/nfc_enhanced.mk)
 
 # Inherit some common Broken stuff.
-$(call inherit-product, vendor/tesla/config/common_full_phone.mk)
-
-# Not actually needed, includes stk (sim toolkit) that is already included in the one just above :)
-#$(call inherit-product, vendor/tesla/config/gsm.mk)
+$(call inherit-product, vendor/exodus/config/common_full_phone.mk)
 
 PRODUCT_DEVICE := hlte
-PRODUCT_NAME := tesla_hlte
+PRODUCT_NAME := exodus_hlte
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1920
