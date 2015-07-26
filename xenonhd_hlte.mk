d1972fde027937cd088e5f18f7af86d3bbfcf939
Ench0 <ench0@users.noreply.github.com>
2015-07-26 20:05:26 +0100

    minimalify

--- xenonhd_hlte.mk
+++ xenonhd_hlte.mk
@@ -2,26 +2,26 @@ $(call inherit-product, device/samsung/hlte/full_hlte.mk)
 
 
 # Inherit common product files.
-$(call inherit-product, vendor/xenonhd/products/common.mk)
+$(call inherit-product, vendor/minimal/products/common.mk)
 
 # Telephony
-$(call inherit-product, vendor/xenonhd/configs/telephony.mk)
+$(call inherit-product, vendor/minimal/configs/telephony.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/xenonhd/configs/nfc_enhanced.mk)
+$(call inherit-product, vendor/minimal/configs/nfc_enhanced.mk)
 
 # Inherit common build.prop overrides
--include vendor/xenonhd/products/common_versions.mk
+-include vendor/minimal/products/common_versions.mk
 
 # Copy hammerhead specific prebuilt files
 PRODUCT_COPY_FILES +=  \
-    vendor/xenonhd/proprietary/tuna/media/bootanimation.zip:system/media/bootanimation.zip \
-    vendor/xenonhd/proprietary/tuna/media/audio/notifications/Nexus.mp3:system/media/audio/notifications/Nexus.mp3 \
-    vendor/xenonhd/proprietary/common/media/LMprec_508.emd:system/media/LMprec_508.emd \
-    vendor/xenonhd/proprietary/common/media/PFFprec_600.emd:system/media/PFFprec_600.emd
+    vendor/minimal/proprietary/tuna/media/bootanimation.zip:system/media/bootanimation.zip \
+    vendor/minimal/proprietary/tuna/media/audio/notifications/Nexus.mp3:system/media/audio/notifications/Nexus.mp3 \
+    vendor/minimal/proprietary/common/media/LMprec_508.emd:system/media/LMprec_508.emd \
+    vendor/minimal/proprietary/common/media/PFFprec_600.emd:system/media/PFFprec_600.emd
 
 # Setup device specific product configuration.
-PRODUCT_NAME := xenonhd_hlte
+PRODUCT_NAME := minimal_hlte
 PRODUCT_BRAND := samsung
 PRODUCT_DEVICE := hlte
 PRODUCT_MODEL := SM-N9005
